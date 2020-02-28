5a3397f75c5275afc54f4b13dfc9ddafc951888e
Melissa Smith <melissa_smith_arizona@yahoo.com>
2020-02-27 20:57:51 -0700

    updated removed coverageChecklist

---
+++ src/main/java/com/example/GalvanizeCapstone/claimant/Claimant.java
@@ -0,0 +1,15 @@
+//package com.example.GalvanizeCapstone.claimant;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table( name = "claimant")
+//@Data
+//public class Claimant {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private int id;
+//
+//}
